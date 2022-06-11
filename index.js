7b13ce4d09cba941e899eba0696e2d39abca5f63
Emma Repp <emmarepp.draws@gmail.com>
2022-06-10 14:49:50 -1000

    commit tree e388d10de1c526e95dc33f1ff2da98fdf4e592d2

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+   return `This Saturday, I want to ${activity}!`;}
+
+function mondayWork(todo = "go to the office") {
+return `This Monday, I will ${todo}.`;}
+
+    function wrapAdjective(flair = '*'){
+   return function(otherWord= 'special'){
+            return `You are ${flair}${otherWord}${flair}!`
+        }
+                }
+            
